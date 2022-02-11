2d16b999f22fb6151d942ed405ede0d7fc5179f3
Javier Peraza <contacto.peraza@gmail.com>
2022-02-11 16:11:45 -0600

    listo para implementar en la clase principal

--- src/Main.java
+++ src/Main.java
@@ -2,24 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        /*float ladoA,ladoB;
-
-        Scanner entradaT = new Scanner(System.in);
-        OperacionGeometrica rectangulo;
-
-        System.out.println("digite el lado A: ");
-        ladoA = entradaT.nextFloat();
-        System.out.println("digite el lado B: ");
-        ladoB = entradaT.nextFloat();
-        //si es cuadrado
-        if (ladoA == ladoB) rectangulo = new OperacionGeometrica(ladoA);
-        //si es rectángulo
-        else rectangulo = new OperacionGeometrica(ladoA, ladoB);
-
-        System.out.println("el perimetro de su cuadrilátero es: " + rectangulo.getPerimetro());
-        System.out.println("el area de su cuadrilátero es: " + rectangulo.getArea());
 
 
-         */
     }
 }
--- src/Rectangulo.java
+++ src/Rectangulo.java
@@ -1,11 +1,33 @@
 public class Rectangulo extends Cuadrilatero {
-    
+    //atributos
+    private float lado1;
+    private float lado2;
+    private float area;
+    private float perimetro;
+
+    //métodos
     @Override
     public void CalcularArea() {
-
+        area = lado1*lado2;
     }
     @Override
     public void CalcularPerimetro() {
+        perimetro = 2*(lado1+lado2);
+    }
 
+    public float getLado1() {
+        return lado1;
+    }
+
+    public void setLado1(float lado1) {
+        this.lado1 = lado1;
+    }
+
+    public float getLado2() {
+        return lado2;
+    }
+
+    public void setLado2(float lado2) {
+        this.lado2 = lado2;
     }
 }
